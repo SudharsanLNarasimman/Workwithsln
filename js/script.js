4557ece1999a024460409501d21be8d7e8933482
SudharsanLNarasimman <167100232+SudharsanLNarasimman@users.noreply.github.com>
2024-04-16 18:08:57 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 
+let texts = ['வணக்கம்', 'హలో', 'ഹലോ', 'नमस्ते','Bonjour','مرحبًا','Hola','ನಮಸ್ಕಾರ','Hi There']; // Array of texts to display
+let index = 0; // Current index of the text
+
 
 
 function menuToggle(){
     $(".menudiv").toggleClass("hidden");
 }
+
+function changeText() {
+    try {
+        document.getElementsByClassName('hithere')[0].innerHTML = texts[index] + ', I\'m <span class="highlight">Sudharsan</span>' ; // Set the text
+        index = (index + 1) % texts.length; // Increment index, loop back to 0 if reached the end
+    } catch (error) {
+        
+    }
+   
+}
+
+setInterval(changeText, 3000);
